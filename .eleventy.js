0df099aaa146bfe53c9c7ec25247916c03272920
ackers93 <andrew.ackers@gmail.com>
2025-08-15 16:06:28 -0700

    homepage layout

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy({ "src/assets/images": "images" });
+
+  eleventyConfig.addFilter("year", () => new Date().getFullYear());
 
   // Watch for CSS changes
   eleventyConfig.addWatchTarget("./src/css/main.css");
@@ -14,7 +17,8 @@ module.exports = function(eleventyConfig) {
       input: "src",
       output: "_site",
       includes: "_includes",
-      layouts: "_layouts"
+      layouts: "_layouts",
+      data: "_data"
     },
     templateFormats: ["html", "md", "njk"],
     htmlTemplateEngine: "njk",
